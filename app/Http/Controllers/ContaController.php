78410ad7cbcd25c5507071c1b437d13bf923f3b0
DionCstS <dionathacastro1@gmail.com>
2024-10-30 17:53:02 -0300

    criado funcao paralistar no controller

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class ContaController extends Controller
 {
-    //
+    // Listar as contas
+
+    public function index() {
+        dd("Listar");
+    }
+
+    //Detalhes da conta
+
+    //Carregar formulário cadastrar nova conta
+
+    //Cadastrar no banco de dados nova conta 
+
+    //Carregar o formulario editar a conta
+
+    //Editar no banco de dados a conta 
+
+    //Excluir a conta do banco de dados 
 }
