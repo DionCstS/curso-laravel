aa16eae97395cf532f5e75fded64f33d4a7d0f85
DionCstS <dionathacastro1@gmail.com>
2024-10-30 18:20:40 -0300

    criacao de rota que chama o ContaController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index-conta', [ContaController::class, 'index'])->name('index.conta');
