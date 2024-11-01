a945eab52b90c1a46db3f2617fcac7e169256564
DionCstS <dionathacastro1@gmail.com>
2024-11-01 15:29:26 -0300

    adcionado rota para acao create

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ContaController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inicio');
 
 Route::get('/index-conta', [ContaController::class, 'index'])->name('index.conta');
+Route::get('/index-create', [ContaController::class, 'create'])->name('create.conta');
