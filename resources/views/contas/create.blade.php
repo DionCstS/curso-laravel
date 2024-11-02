aa4dcd261d0e7fe10c792a7be27a5739f873b3bb
DionCstS <dionathacastro1@gmail.com>
2024-11-02 15:07:45 -0300

    criacao de formulario de cadastro

--- resources/views/contas/create.blade.php
+++ resources/views/contas/create.blade.php
@@ -8,5 +8,25 @@
 <body>
     <a href="{{ route('index.conta') }}">Listar</a>
     <h2>Criar conta</h2>
+
+    <div action="" method="POST">
+        @csrf
+        
+        <label for="">Nome</label>
+        <input type="text" name="nome" id="nome" placeholder="Digite o nome" required><br><br>
+
+        <label for="">Valor</label>
+        <input type="text" name="valor" id="valor" placeholder="Digite o valor com '.'" required><br><br>
+
+        <label for="">Vencimento</label>
+        <input type="date" name="vencimento" id="vencimento" required><br><br>
+
+        <button type="submit">Cadastrar</button>
+
+
+        
+
+
+    </div>
 </body>
 </html>
