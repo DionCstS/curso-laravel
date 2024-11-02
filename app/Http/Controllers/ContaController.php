f8b96998b4fd0adba79bccbe0a21091c068663eb
DionCstS <dionathacastro1@gmail.com>
2024-11-02 09:49:38 -0300

    adcionando comentarios

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -12,39 +12,51 @@ class ContaController extends Controller
         return view('contas.index');
     }
 
+    //Carregar formulário cadastrar nova conta
+
     public function create() {
         return view('contas.create');
     }
+
+    //Cadastrar no banco de dados nova conta 
 
     public function store() {
         return dd("Cadastrar");
     }
 
+    //Detalhes da Conta
+
     public function show() {
         return view('contas.show');
     }
+
+    //Carregar o formulario editar a conta
 
     public function edit() {
         return view('contas.edit');
     }
 
+    //Editar no banco de dados a conta 
+
     public function update() {
         return dd("Atualizar");
     }
+
+    //Excluir a conta do banco de dados 
 
     public function destroy() {
         return dd("Deletar");
     }
 
-    //Detalhes da conta
+    
 
-    //Carregar formulário cadastrar nova conta
+    
 
-    //Cadastrar no banco de dados nova conta 
+    
 
-    //Carregar o formulario editar a conta
+    
 
-    //Editar no banco de dados a conta 
+    
 
-    //Excluir a conta do banco de dados 
+    
 }
