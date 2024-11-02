2fa7bd37f5f93bf52226fb76a93dcc1e83a9fc8e
DionCstS <dionathacastro1@gmail.com>
2024-11-02 15:31:21 -0300

    indicar campos para serrem prenchidos

--- app/Models/Conta.php
+++ app/Models/Conta.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conta extends Model
 {
-    //
+    //Indicar o nome da tabela
+
+    protected $table = 'contas';
+
+    //Indicar quais campos podem ser prenchidos
+
+    protected $fillable = ['nome', 'valor', 'vencimento'];
 }
