feda67f8475b45c9bd6f881558e4fc856536e33d
DionCstS <dionathacastro1@gmail.com>
2024-11-04 14:02:39 -0300

    mensagem na view de concluido com cor verde

--- resources/views/contas/show.blade.php
+++ resources/views/contas/show.blade.php
@@ -8,8 +8,11 @@
 <body>
     <a href="{{ route('index.conta') }}">Listar</a>
     <h2>Detalhes da Conta</h2>
-    @if ( session('success') ) 
-        {{ session('success') }}
+    @if ( session('success') )
+    <span  style="color: #008000">
+         {{ session('success') }} 
+    </span>
+        
     @endif
 
 </body>
